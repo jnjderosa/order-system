49f3309f0256bcb3f145e2222b3c377314956424
jnjderosa <jnjderosa@earthlink.net>
2016-03-03 09:33:40 -0800

    add integ test

---
+++ src/test/java/com/mulesoft/training/HelloMavenTestITCase.java
@@ -0,0 +1,28 @@
+package com.mulesoft.training;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class HelloMavenTestITCase extends FunctionalTestCase {
+
+    @Test
+    public void mavenFlowReturnsHelloMaven() throws Exception {
+        runFlowAndExpect("helloFlow", "hello");
+    }
+  
+/**  @Test
+    public void retrieveOrdersAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("processOrders");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }**/
+
+    @Override
+    protected String getConfigFile() {
+        return "order-system.xml";
+    }
+
+}
